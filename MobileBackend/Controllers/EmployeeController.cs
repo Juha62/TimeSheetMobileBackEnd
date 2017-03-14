08a2ab05a0282a241dc8111cc5034c7c9b3c0465
Jani Järvinen <jani.jarvinen@intertechno.org>
2017-03-14 09:53:27 +0200

    Lisätty mobiilisovelluksen taustajärjestelmä GitHubiin.

---
+++ MobileBackend/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using MobileBackend.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MobileBackend.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public string[] GetAll()
+        {
+            string[] employeeNames = null;
+            TimesheetEntities entities = new TimesheetEntities();
+            try
+            {
+                employeeNames = (from e in entities.Employees
+                                 where (e.Active == true)
+                                 select e.FirstName + " " +
+                                 e.LastName).ToArray();
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+
+            return employeeNames;
+        }
+    }
+}

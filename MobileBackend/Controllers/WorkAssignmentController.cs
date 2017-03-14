b596ec80478988802521c0b846016553fe5542fb
Jani Järvinen <jani.jarvinen@intertechno.org>
2017-03-14 14:35:23 +0200

    Lisätty työtehtävän aloittamisen tallentaminen tietokantaan taustajärjestelmään.

--- MobileBackend/Controllers/WorkAssignmentController.cs
+++ MobileBackend/Controllers/WorkAssignmentController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TimesheetMobile.Models;
 
 namespace MobileBackend.Controllers
 {
@@ -26,6 +27,46 @@ namespace MobileBackend.Controllers
             }
 
             return assignmentNames;
+        }
+
+        [HttpPost]
+        public bool PostStatus(WorkAssignmentOperationModel input)
+        {
+            TimesheetEntities entities = new TimesheetEntities();
+            try
+            {
+                WorkAssignment assignment = (from wa in entities.WorkAssignments
+                                             where (wa.Active == true) &&
+                                             (wa.Title == input.AssignmentTitle)
+                                             select wa).FirstOrDefault();
+
+                if (assignment == null)
+                {
+                    return false;
+                }
+
+                int assignmentId = assignment.Id_WorkAssignment;
+
+                Timesheet newEntry = new Timesheet()
+                {
+                    Id_WorkAssignment = assignmentId,
+                    StartTime = DateTime.Now,
+                    Active = true,
+                    CreatedAt = DateTime.Now
+                };
+                entities.Timesheets.Add(newEntry);
+                entities.SaveChanges();
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+
+            return true;
         }
     }
 }

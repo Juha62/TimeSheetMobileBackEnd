83565605d0a3d600c310283672d6a15a28892a2a
Jani Järvinen <jani.jarvinen@intertechno.org>
2017-03-14 11:07:30 +0200

    Lisätty SQL-tietokannan rakenne ja luontiskripti versionhallintaan.

---
+++ MobileBackend/Controllers/WorkAssignmentController.cs
@@ -0,0 +1,31 @@
+﻿using MobileBackend.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MobileBackend.Controllers
+{
+    public class WorkAssignmentController : ApiController
+    {
+        public string[] GetAll()
+        {
+            string[] assignmentNames = null;
+            TimesheetEntities entities = new TimesheetEntities();
+            try
+            {
+                assignmentNames = (from wa in entities.WorkAssignments
+                                   where (wa.Active == true)
+                                   select wa.Title).ToArray();
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+
+            return assignmentNames;
+        }
+    }
+}

03da983669514b214a23424e6bb1db916577e666
Jani Järvinen <jani.jarvinen@intertechno.org>
2017-03-21 14:32:31 +0200

    Lisätty yksinkertainen työntekijän kuvan lataus- ja tallennustoiminto taustajärjesetlmään.

--- MobileBackend/Controllers/EmployeeController.cs
+++ MobileBackend/Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using MobileBackend.DataAccess;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -27,6 +28,52 @@ namespace MobileBackend.Controllers
             }
 
             return employeeNames;
+        }
+
+        public byte[] GetEmployeeImage(string employeeName)
+        {
+            TimesheetEntities entities = new TimesheetEntities();
+            try
+            {
+                string[] nameParts = employeeName.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                string first = nameParts[0];
+                string last = nameParts[1];
+                byte[] bytes = (from e in entities.Employees
+                                where (e.Active == true) &&
+                                (e.FirstName == first) &&
+                                (e.LastName == last)
+                                select e.EmployeePicture).FirstOrDefault();
+
+                return bytes;
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+        }
+
+        public string PutEmployeeImage()
+        {
+            TimesheetEntities entities = new TimesheetEntities();
+            try
+            {
+                Employee newEmployee = new Employee()
+                {
+                    FirstName = "Heebo",
+                    LastName = "X",
+                    EmployeePicture = File.ReadAllBytes(@"C:\Temp\Heebo.png")
+                };
+                entities.Employees.Add(newEmployee);
+                entities.SaveChanges();
+
+                return "OK!";
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+
+            return "Error";
         }
     }
 }
--- MobileBackend/DataAccess/TimesheetModel.Designer.cs
+++ MobileBackend/DataAccess/TimesheetModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\oppiadmin\Source\Repos\MobileBackend\MobileBackend\DataAccess\TimesheetModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\oppiadmin\Source\Repos\MobileBackend\TimesheetMobileBackend\MobileBackend\DataAccess\TimesheetModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
